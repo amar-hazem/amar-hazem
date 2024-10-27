b77a0bb516a558f955a70f1dc9e067e0cd464c11
Amar Hazem <amar.hazem@icloud.com>
2024-10-27 16:42:03 +0100

    feat(prettier-config): 6-add-prettier

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,29 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  experimentalTernaries: false,
+  // filepath: null,
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxSingleQuote: false,
+  // parser: null,
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  rangeEnd: Infinity,
+  rangeStart: 0,
+  requirePragma: false,
+  semi: true,
+  singleAttributePerLine: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+};
+
+export default config;
