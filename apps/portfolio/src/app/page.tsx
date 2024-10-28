c92c339f3aaccde5750fa23bd5980765d7b92b7f
Amar Hazem <amar.hazem@icloud.com>
2024-10-28 23:41:07 +0100

    feat(ah): 8 Add postgresql service in docker-compose

---
+++ apps/portfolio/src/app/page.tsx
@@ -0,0 +1,7 @@
+import { JSX } from "react";
+
+export default function RootPage(): JSX.Element {
+  return (
+    <></>
+  );
+}
--- apps/web/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: ["@ah/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-  root: true,
-};
